15a3bc15c490f74269bf16c262cf0ab9e4d3085b
jamesdoconnr <jamesdoconnor2@gmail.com>
2023-09-19 08:51:56 -0400

    Added hello_strings

---
+++ Exercises/ch07/hello_strings.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string string1;
+    	string1  = "Bonjour, ";
+    	string string2 = "strings!";
+    	cout << string1 << string2 << endl;
+    	return 0;
+}
