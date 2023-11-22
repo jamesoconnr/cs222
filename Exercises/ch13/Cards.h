6c8fd00e1b7eff0db9039a3aac33b24202b2eff7
James Oconnor <jamesdoconnor2@gmail.com>
2023-11-22 14:55:54 -0500

    Finished ch13 exercise 1

---
+++ Exercises/ch13/Cards.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Card
+{
+    int suit, rank;
+
+    Card();
+    Card(int s, int r);
+    string to_string() const;
+    bool is_greater(const Card& c2) const;
+    bool equals(const Card& c2) const;
+};
---
+++ Exercises/ch13/cards.cpp
@@ -0,0 +1,52 @@
+#include "Cards.h"
+
+Card::Card() {
+    suit = 0; rank = 0;
+}
+
+Card::Card(int s, int r) {
+    suit = s; rank = r;
+}
+
+string Card::to_string() const
+{
+    vector<string> suit_strings = {"Clubs", "Diamonds", "Hearts", "Spades"};
+    vector<string> rank_strings = {"Joker", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King",  "Ace"};
+
+    if (rank == 0) return rank_strings[rank];
+    return rank_strings[rank] + " of " + suit_strings[suit];
+}
+
+bool Card::equals(const Card& c2) const
+{
+    return (rank == c2.rank && suit == c2.suit);
+}
+
+bool Card::is_greater(const Card& c2) const
+{
+    if (suit > c2.suit) return true;
+    if (suit < c2.suit) return false;
+
+    if (rank > c2.rank) return true;
+    if (rank < c2.rank) return false;
+    return false;
+}
+
+int main(){
+    Card myCard(1, 2);
+    Card myCard2(3, 4);
+    cout << myCard.to_string() << endl;
+    if (myCard.is_greater(myCard2)) {
+        cout << myCard.to_string() << " is greater than " << myCard2.to_string();
+        cout << endl;
+    } else {
+        cout << "Not equal" << endl;
+    }  
+    if (myCard.equals(myCard2)) {
+        cout << "Yup, that's the same card." << endl;
+    } else {
+        cout << "Not the same cards" << endl;
+    }
+
+    return 1;
+}
